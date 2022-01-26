d9219e5f5b473988b4571fa522dfc2071b5af7c6
takumi3110 <takumi.s5296@gmail.com>
2022-01-26 10:42:09 +0900

    add:intro function

--- 04_javascript_basic/025_intro_function/main.js
+++ 04_javascript_basic/025_intro_function/main.js
@@ -1,7 +1,18 @@
-//let name = 'TOM';
-let name = 'Tom';
-name = 'Tim'
+const hello2 = function(name, age) {
+    // let name = 'Watame';
+    console.log('hello ' + name + age);
+    return name + age
+}
 
-console.log('hello' + name);
+// 無名関数 callback関数
+function hello(name, age) {
+    // let name = 'Watame';
+    console.log('hello ' + name + age);
+    return name + age
+}
 
+hello('Watame', 10);
+const returnVal = hello('Botan', 10);
+console.log(returnVal);
 
+hello2('noel', 20)
