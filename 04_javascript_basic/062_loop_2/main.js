23698c399055d804848ad1068f1423d5f2e4a1da
takumi3110 <takumi.s5296@gmail.com>
2022-01-27 10:22:11 +0900

    add:loop

---
+++ 04_javascript_basic/062_loop_2/main.js
@@ -0,0 +1,21 @@
+const todos = [
+    {
+        id: 1,
+        title:'Go to school',
+        completed: true,
+    },
+    {
+        id: 2,
+        title:'Go to museum',
+        completed: true,
+    },
+    {
+        id: 3,
+        title:'Go to shopping',
+        completed: true,
+    }
+]
+
+for (let i = 0; i < todos.length; i++) {
+    console.log(todos)
+}
