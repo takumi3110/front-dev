28cf606781471dbfe198fb124685d2552b368995
takumi3110 <takumi.s5296@gmail.com>
2022-02-11 12:20:43 +0900

    add:finished slider

--- 05_javascript_practice/070_slider_animation/main.js
+++ 05_javascript_practice/070_slider_animation/main.js
@@ -3,20 +3,4 @@ const swiper = new Swiper('.swiper', {
     // direction: 'vertical',
     loop: true,
     effect: 'coverflow',
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
 });
