e09cc764f27d07a47daffc9af68db954821531ba
takumi3110 <takumi.s5296@gmail.com>
2022-02-15 10:45:37 +0900

    refactor:フォルダ構成完了

---
+++ 99_sample_site/scripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hero = new HeroSlider('.swiper');
+    hero.start();
+    //
+    // setTimeout(() => {
+    //     hero.stop();
+    // }, 5000);
+});
