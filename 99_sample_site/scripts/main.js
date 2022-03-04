97964b1d2012cfc435ee8ccf6ed07029c03182db
takumi3110 <takumi.s5296@gmail.com>
2022-03-04 11:20:31 +0900

    add:text-animation

--- 99_sample_site/scripts/main.js
+++ 99_sample_site/scripts/main.js
@@ -74,6 +74,7 @@ class Main {
         this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
         this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation);
         this.observers = new ScrollObserver('.swiper', this._toggleSlideAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('.appear', this._inviewAnimation);
         console.log(this.observers);
     }
 
